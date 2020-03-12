84b981672183967e79d91e98d1108c8b11440ab7
nconde77 <nicolas.conde@vera.com.uy>
2020-03-11 23:52:04 -0300

    Cambio de Gson a json-simple.\nAgregué HashMap de partidas.

--- ude/proyecto3/Servidor/Servidor.java
+++ ude/proyecto3/Servidor/Servidor.java
@@ -1,11 +1,9 @@
 package ude.proyecto3.Servidor;
 
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-
-import ude.proyecto3.Servidor.Persistencia.IFabrica;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -13,20 +11,22 @@ import java.io.IOException;
 import java.net.InetAddress;
 import java.sql.SQLException;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Properties;
 import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
 import javax.websocket.*;
 import javax.websocket.server.ServerEndpoint;
+
 import ude.proyecto3.Servidor.Logica.EstadoPartida;
 import ude.proyecto3.Servidor.Logica.Jugador;
 import ude.proyecto3.Servidor.Logica.Partida;
 import ude.proyecto3.Servidor.Persistencia.IConexion;
 import ude.proyecto3.Servidor.Persistencia.IDAOJugador;
 import ude.proyecto3.Servidor.Persistencia.IDAOPartida;
+import ude.proyecto3.Servidor.Persistencia.IFabrica;
 import ude.proyecto3.Servidor.Persistencia.IPoolConexiones;
 
 
@@ -36,6 +36,7 @@ public class Servidor {
 	// Para poder loguear.
 	Logger logger = Logger .getLogger(Servidor.class.getName());
 	private static Set<Session> sesiones = Collections.synchronizedSet(new HashSet<Session>());
+	private HashMap<Long, Partida> partidas = new HashMap<Long, Partida>(); 
 	private IPoolConexiones ipool;
 	private IDAOJugador jugPersistencia;
 	private IDAOPartida partPersistencia;
@@ -69,8 +70,6 @@ public class Servidor {
 	 */
 	@OnOpen
 	public void alAbrir(Session sesion) {
-		System.out.println("Abriendo sesión " + sesion.getId() + ".");
-		System.err.println("Abriendo sesión " + sesion.getId() + ".");
 		logger.log(Level.INFO, "Abriendo sesión " + sesion.getId() + ".");
 		sesiones.add(sesion);
 	}	// alAbrir
@@ -82,24 +81,28 @@ public class Servidor {
 	 * @param mensaje Una cadena de texto que contiene el mensaje JSON del cliente con un tipo y los datos necesarios para procesarlo.
 	 */
 	@OnMessage
-	public void cuandoMensaje(Session sesion, String mensaje) throws FileNotFoundException, SQLException, IOException {
-		JsonParser jParse = new JsonParser();
-		JsonObject jObj;
+	public void cuandoMensaje(Session sesion, String mensaje) throws SQLException, ParseException, IOException {
+		JSONParser jParser = new JSONParser();
+		JSONObject jObj;
 		
-		System.out.println("Hay mensaje " + mensaje + ".\n");
-		System.err.println("Hay mensaje " + mensaje + ".\n");
-
-		for (Session s : sesiones) {
-			if (!s.equals(sesion)) {
-				logger.log(Level.INFO, "Sesión " + s.getId() + ".\n");
-				s.getBasicRemote().sendText(mensaje);
-			}	// if
-		}	// for
+		logger.log(Level.INFO, "Mensaje " + mensaje + " de " + sesion.getId() + ".\n");
 		
-		jObj = jParse.parse(mensaje).getAsJsonObject();
-		switch (jObj.get("tipo").getAsString()) {
+		/* Se cambió de clase para parsear a json-simple en vez de Gson
+		 * porque si no revienta como chinche.
+		 */
+		jObj = (JSONObject) jParser.parse(mensaje);
+		
+		switch ((String) jObj.get("tipo")) {
+			case "MOVIMIENTO":
+				for (Session s : sesiones) {
+					if (!s.equals(sesion)) {
+						// logger.log(Level.INFO, "Sesión " + s.getId() + ".\n");
+						s.getBasicRemote().sendText(mensaje);
+					}	// if
+				}	// for
+				break;
 			case "CREAR_PART":
-				crearPartida(jObj.get("nombre").getAsString(), jObj.get("bando").getAsString(), -1,
+				crearPartida((String) jObj.get("nombre"), (String) jObj.get("bando"), -1,
 					0, 0, EstadoPartida.CREADA, PE_MAX_COMB, PA_MAX_COMB, MAX_TIEMPO);
 				break;
 			case "INI_PART":
@@ -109,7 +112,7 @@ public class Servidor {
 			case "FIN_PART":
 				break;
 			default:
-				System.err.println("Mensaje desconocido " + mensaje + ".\n");
+				logger.log(Level.WARNING, "Mensaje desconocido " + mensaje + ".\n");
 				break;
 		}	// switch
 	}	// cuandoMensaje
@@ -126,6 +129,7 @@ public class Servidor {
 		Partida part;
 		part = new Partida(nom, bando, id, ptosJPat, ptosJPes, estado, combusJPes, combusJPat, tiempo);
 		partPersistencia.guardarPartida(con, part);
+		partidas.put(part.getId(), part);
 		ipool.liberarConexion(con, true);
 	}	// crear Partida
 	
@@ -138,13 +142,6 @@ public class Servidor {
 		
 		partPersistencia.guardarPartida(con, part);
 		ipool.liberarConexion(con, true);
-//		jPat = part.getJpat();
-//		jPes = part.getJpes();
-//		jPat.sumarPuntos(part.getPtosJPat()); 
-//		jPes.sumarPuntos(part.getPtosJPes());
-
-		//part.setEstadoPartida(EstadoPartida.TERMINADA);
-		
 	}	// guardarPartida
 	
 	public void iniciarPartida(int id, String estado) throws SQLException {
@@ -153,7 +150,6 @@ public class Servidor {
 		part = partPersistencia.encontrar(con, id);
 		part.setEstadoPartida(EstadoPartida.INICIADA);
 		ipool.liberarConexion(con, true);
-		
 	}	// iniciarPartida
 	
 	public void pausarPartida(Partida part) throws FileNotFoundException, IOException { //String nom, String estado
@@ -181,19 +177,19 @@ public class Servidor {
 		jPat.sumarPuntos(part.getPtosJPat()); 
 		jPes.sumarPuntos(part.getPtosJPes());
 		part.setEstadoPartida(EstadoPartida.TERMINADA);
+		partidas.remove(part.getId());
 		ipool.liberarConexion(con, true);
 	}	// terminarPartida
 		
 	@OnError
 	public void cuandoError(Throwable t) {
-		System.out.println("Error:" + t.getMessage());
+		logger.log(Level.SEVERE, "cuandoError: " + t.getMessage());
 	}	// cuandoError
 	
 	
 	@OnClose
-	public void alCerrar(Session sesion) {
-		System.out.println("Cerrando sesión " + sesion.getId() + ".");
-		logger.log(Level.INFO, "Cerrando sesión " + sesion.getId() + ".");
+	public void alCerrar(Session sesion, CloseReason cr) {
+		logger.log(Level.INFO, "Cerrando sesión " + sesion.getId() + " por " + cr.toString() + ".\n");
 		sesiones.remove(sesion);
 	}	// alCerrar
 	
